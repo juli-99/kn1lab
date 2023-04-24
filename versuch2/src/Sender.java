3c64a0db2a1817d614cbc12abb82be91b92df58f
pmmader <116314501+pmmader@users.noreply.github.com>
2023-04-24 14:06:13 +0200

    Update Sender.java

--- versuch2/src/Sender.java
+++ versuch2/src/Sender.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.*;
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.concurrent.*;
 
 /**
@@ -30,16 +31,46 @@ public class Sender {
      * @throws IOException Wird geworfen falls Sockets nicht erzeugt werden können.
      */
     private void send() throws IOException {
-/*   	//Text einlesen und in Worte zerlegen
-
+    	//Text einlesen und in Worte zerlegen
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String in = br.readLine();
+        in += " EOT";
+        String[] words = in.split(" ");
         // Socket erzeugen auf Port 9998 und Timeout auf eine Sekunde setzen
-
+        DatagramSocket clientSocket = new DatagramSocket(9998);
+        clientSocket.setSoTimeout(1000);
         // Iteration über den Konsolentext
-        while (true) {
+        int index = 0;
+        int seq = 1;
+        int ackNum = 1;
+        while (index < words.length) {
+            String w = words[index];
         	// Paket an Port 9997 senden
-        	
+            Packet packetOut = new Packet(seq, ackNum, true, w.getBytes());
+            int tmp = seq + w.getBytes().length;
+
+            ByteArrayOutputStream b = new ByteArrayOutputStream();
+            ObjectOutputStream o = new ObjectOutputStream(b);
+            o.writeObject(packetOut);
+            byte[] buf = b.toByteArray();
+
+            DatagramPacket sndPacket = new DatagramPacket(buf, buf.length, InetAddress.getLocalHost(), 9997);
+            clientSocket.send(sndPacket);
+
             try {
                 // Auf ACK warten und erst dann Schleifenzähler inkrementieren
+                byte[] buf_rcv = new byte[256];
+                DatagramPacket rcvPacketRaw = new DatagramPacket(buf_rcv, buf_rcv.length);
+                clientSocket.receive(rcvPacketRaw);
+
+                ObjectInputStream i = new ObjectInputStream(new ByteArrayInputStream(rcvPacketRaw.getData()));
+                Packet packetIn = (Packet) i.readObject();
+
+                if (packetIn.getAckNum() == tmp && packetIn.isAckFlag()) {
+                    seq = tmp;
+                    ackNum += packetIn.getPayload().length;
+                    index++;
+                }
 
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
@@ -54,7 +85,7 @@ public class Sender {
         if(System.getProperty("os.name").equals("Linux")) {
             clientSocket.disconnect();
         }
-*/
+
         System.exit(0);
     }
 }
